66b0716f3e26e0c3af427b38e314977fd366b284
ranaolwan <137338453+ranaolwan@users.noreply.github.com>
2023-12-10 01:16:27 +0000

    Java Script added with some styling css

---
+++ finalproject-final/assets/js/script.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+
+  $('#info').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+
+});
+
+
+$(document).ready(function() {
+
+  $('#pic').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
